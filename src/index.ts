211a867de5c023fd433af2cf684d813c4cdf05b5
DevTools.at <devtools@devtools.at>
2025-12-12 13:00:35 +0100

    Initial commit - npm package with TypeScript support

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Diff Checker
+ * Compare two texts and find differences
+ *
+ * Online tool: https://devtools.at/tools/diff-checker
+ *
+ * @packageDocumentation
+ */
+
+function calculateDiff(
+  text1: string,
+
+// Export for convenience
+export default { encode, decode };
